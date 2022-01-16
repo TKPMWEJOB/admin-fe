6b56b742960e8b6df782bf3192c561f23ff8e339
Phu Do <dophu1402@gmail.com>
2022-01-16 16:04:06 +0700

    list course

---
+++ src/components/tableHeaders/CourseTableHeader.js
@@ -0,0 +1,71 @@
+import { Tooltip } from "@mui/material";
+
+const headers = [
+    { field: 'id', width: "20" },
+    {
+        field: 'name', headerName: "Course name", width: "300",
+        renderCell: (params) => (
+            <Tooltip title={params.value} >
+                <span className="table-cell-trucate">{params.value}</span>
+            </Tooltip>
+        ),
+    },
+    {
+        field: 'room',
+        renderCell: (params) => (
+            <Tooltip title={params.value} >
+                <span className="table-cell-trucate">{params.value}</span>
+            </Tooltip>
+        ),
+    },
+    {
+        field: 'section',
+        renderCell: (params) => (
+            <Tooltip title={params.value} >
+                <span className="table-cell-trucate">{params.value}</span>
+            </Tooltip>
+        ),
+    },
+    {
+        field: 'username', width: "120",
+        renderCell: (params) => (
+            <Tooltip title={params.value} >
+                <span className="table-cell-trucate">{params.value}</span>
+            </Tooltip>
+        ),
+    },
+    {
+        field: 'owner', width: "150",
+        renderCell: (params) => (
+            <Tooltip title={params.value} >
+                <span className="table-cell-trucate">{params.value}</span>
+            </Tooltip>
+        ),
+    },
+    {
+        field: 'ownerEmail', headerName: "Email", width: "200",
+        renderCell: (params) => (
+            <Tooltip title={params.value} >
+                <span className="table-cell-trucate">{params.value}</span>
+            </Tooltip>
+        ),
+    },
+    {
+        field: 'createdAt', headerName: "Created Date", width: "120",
+        renderCell: (params) => (
+            <Tooltip title={params.value} >
+                <span className="table-cell-trucate">{params.value}</span>
+            </Tooltip>
+        ),
+    },
+    {
+        field: 'updatedAt', headerName: "Updated Date", width: "120",
+        renderCell: (params) => (
+            <Tooltip title={params.value} >
+                <span className="table-cell-trucate">{params.value}</span>
+            </Tooltip>
+        ),
+    },
+]
+
+export default headers;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,50 @@
+import { RemoveFromQueue } from "@mui/icons-material";
+import { DataGrid } from "@mui/x-data-grid"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import headers from '../components/tableHeaders/CourseTableHeader'
 
 export default function Courses() {
+  const [data, setData] = useState([]);
+
+  useEffect(async () => {
+    try {
+      const res = await axios.get(`${process.env.REACT_APP_API_URL}/admin/courses`);
+      const data = res.data.rows;
+      console.log(data);
+      const courses = data.map((row) => {
+        const ownerName = `${row.owner.firstName ? row.owner.firstName : ''} ${row.owner.lastName ? row.owner.lastName : ''}`
+        let course = {
+          id: row.id,
+          name: row.name,
+          room: row.room,
+          section: row.section,
+          username: row.username,
+          owner: ownerName,
+          ownerId: row.ownerId,
+          ownerEmail: row.owner.email,
+          createdAt: new Date(row.createdAt).toString(),
+          updatedAt: new Date(row.updatedAt).toString(),
+        }
+        return course;
+      })
+      setData(courses);
+    } catch (error) {
+    }
+  }, [])
+
+
+
   return (
-    <div>
-      Dashboard
+    <div style={{ padding: '20px' }}>
+      <DataGrid
+        style={{ height: '800px' }}
+        hideFooterPagination
+        scrollbarSize={5}
+        columns={headers}
+        rows={data}
+      />
+
     </div>
   )
 }
